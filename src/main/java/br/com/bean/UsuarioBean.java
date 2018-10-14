e552569e8193358b5d34483d51884bfa55ec4f94
Denis <Denis@DESKTOP-HHSFAR8>
2018-10-14 12:32:15 -0300

    tela cadastro aluno

--- src/main/java/br/com/bean/UsuarioBean.java
+++ src/main/java/br/com/bean/UsuarioBean.java
@@ -5,7 +5,10 @@
  */
 package br.com.bean;
 
+import br.com.dao.AlunoDao;
+import br.com.modelo.Aluno;
 import br.com.modelo.Usuario;
+import br.com.util.Utilidades;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 
@@ -13,10 +16,24 @@ import javax.faces.bean.ManagedBean;
 public class UsuarioBean implements Serializable {
 
     private Usuario usu = new Usuario();
+    private Aluno al = new Aluno();
+    private Utilidades util = new Utilidades();
 
     public String inserir() {
 
         return "cadastro";
+    }
+
+    public String logar() {
+        boolean test = util.valida(al);
+
+        if (test == true) {
+            return "index?faces-redirect=true";
+        } else {
+            return "/adm/indexAdm";
+            
+        }
+
     }
 
     //getter e setter
@@ -26,6 +43,14 @@ public class UsuarioBean implements Serializable {
 
     public void setUsu(Usuario usu) {
         this.usu = usu;
+    }
+
+    public Utilidades getUtil() {
+        return util;
+    }
+
+    public void setUtil(Utilidades util) {
+        this.util = util;
     }
 
 }
